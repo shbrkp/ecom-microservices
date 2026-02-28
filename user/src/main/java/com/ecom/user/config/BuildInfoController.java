c6297f73d7c1b06b7f1bd2ab7c7cab32661594b2
Shabeer <shbrkp@gmail.com>
2026-02-28 19:33:19 +0530

    profile-section changes commit to local

--- user/src/main/java/com/ecom/user/config/BuildInfoController.java
+++ user/src/main/java/com/ecom/user/config/BuildInfoController.java
@@ -1,23 +1,27 @@
 package com.ecom.user.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class BuildInfoController {
 
-    @Value("${BUILD.ID:default}")
-    private String buildId;
+//    @Value("${BUILD.ID:default}")
+//    private String buildId;
+//
+//
+//    @Value("${JAVA_HOME:default}")
+//    private String buildName;
 
-
-    @Value("${JAVA_HOME:default}")
-    private String buildName;
+    public BuildInfo buildInfo;
 
 //    http://localhost:8082/build-info
     @GetMapping("/build-info")
     public String getbuildInfo(){
-        return "Build Id :"+buildId+"Build Id small  Build Nmae :"+buildName;
+        return "Build Id :"+buildInfo.getId()+"Build Id small  Build Nmae :"+buildInfo.getName();
 
     }
 }
