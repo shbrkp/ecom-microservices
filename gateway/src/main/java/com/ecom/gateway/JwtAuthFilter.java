bd069df25407c56c664e7fec46203a6c89048467
Shabeer <shbrkp@gmail.com>
2026-03-05 14:48:05 +0530

    spring-filter-section changes commit to local including gateway

---
+++ gateway/src/main/java/com/ecom/gateway/JwtAuthFilter.java
@@ -0,0 +1,26 @@
+package com.ecom.gateway;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+//@Component
+public class JwtAuthFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String authHeader = exchange.getRequest().getHeaders()
+                .getFirst(HttpHeaders.AUTHORIZATION);
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        // Code to Validate JWT token has to be writtern here
+        return chain.filter(exchange);
+    }
+}
